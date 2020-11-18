3d49ec96ba9ecb338bbe94bafb822bc049f1ae22
Dawid Komisarczuk <komidawi@gmail.com>
2020-11-18 21:56:15 +0100

    [TAI-20] Feat: Make Zuul fully working

--- project/api-gateway/src/main/java/pl/agh/apigateway/ApiGatewayApplication.java
+++ project/api-gateway/src/main/java/pl/agh/apigateway/ApiGatewayApplication.java
@@ -2,12 +2,21 @@ package pl.agh.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import pl.agh.apigateway.filter.SimpleFilter;
 
+@EnableZuulProxy
 @SpringBootApplication
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+    @Bean
+    public SimpleFilter simpleFilter() {
+        return new SimpleFilter();
     }
 
 }
