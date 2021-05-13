cbac973b028062799240d4bdea6e1dfba51aed4e
Dawid Komisarczuk <komidawi@gmail.com>
2021-05-13 17:56:52 +0200

    Feat: Add random exception throwing and random number endpoint for zuul

--- project/api-gateway/src/main/java/pl/agh/apigateway/SampleController.java
+++ project/api-gateway/src/main/java/pl/agh/apigateway/SampleController.java
@@ -3,11 +3,23 @@ package pl.agh.apigateway;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 @RestController
 public class SampleController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World";
+        int randomInt = ThreadLocalRandom.current().nextInt(1, 100);
+        if ((randomInt % 2) == 0) {
+            return "Hello World";
+        } else {
+            throw new RuntimeException("Random exception: " + randomInt);
+        }
+    }
+
+    @GetMapping("/random-number")
+    public Integer randomNumber() {
+        return ThreadLocalRandom.current().nextInt(-100, 100);
     }
 }
